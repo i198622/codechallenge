fe0b43b495fca5f7dfce770599e543c51af9152b
Алексей Мелекесов <aleksey.melekesov@rt.ru>
2025-04-18 10:46:53 +0400

    Fix code.

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,10 @@
 import { IPull } from "@/type";
-import dump from '../../../../dimp_64_gemini_flash.json';
 import { parallelCodeReview } from "./agent";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { pulls } = body;
   
-  // const text = await parallelCodeReview(pulls as IPull[]);
-  return new Response(JSON.stringify(dump));
+  const text = await parallelCodeReview(pulls as IPull[]);
+  return new Response(JSON.stringify(text));
 }
